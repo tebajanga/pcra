85cc2f72bab5cc661d959e50c54913596a32dab2
Timothy Anthony <anthony.timothy90@gmail.com>
2018-10-16 16:12:38 +0200

    Initial project baseline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'React2',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 1,
+  },
+  {
+    title: 'React3',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 2,
+  }
+];
+
+/*
+function isSearched(searchTerm) {
+  return function (item) {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+}*/
+
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+      searchTerm: '',
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  onDismiss(id) {
+    /*
+    function isNotId(item){
+      return item.objectID !== id;
+    }*/
+
+    // const updateList = this.state.list.filter(item => item.objectID !== id);
+
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value })
+  }
+
   render() {
+    // Destructuring
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form>
+          <input 
+            type="text"
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          />
+        </form>
+        {list.filter(isSearched(searchTerm)).map(item =>
+          <div key={item.objectID}>
+            <span><a href="{item.url}">{item.title}</a>&nbsp;&nbsp;</span>
+            <span>{item.author}&nbsp;&nbsp;</span>
+            <span>{item.num_comments}&nbsp;&nbsp;</span>
+            <span>{item.points}&nbsp;&nbsp;</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
+            <br /><br />
+          </div>
+        )}
       </div>
     );
   }
