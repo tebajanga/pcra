de487abd4c1575695dbd3e9ca82a2725d86f337a
Timothy Anthony <anthony.timothy90@gmail.com>
2018-10-17 15:06:08 +0200

    Handling error

--- src/App.js
+++ src/App.js
@@ -79,6 +79,7 @@ class App extends Component {
       results: null,
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null,
     };
 
     this.onDismiss = this.onDismiss.bind(this);
@@ -147,7 +148,7 @@ class App extends Component {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+      .catch(error => this.setState({ error }));
   }
 
   componentDidMount() {
@@ -160,7 +161,8 @@ class App extends Component {
     const { 
       searchTerm, 
       results,
-      searchKey
+      searchKey,
+      error
     } = this.state;
 
     const page = (
@@ -186,10 +188,15 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table
-          list={list}
-          onDismiss={this.onDismiss}  
-        />
+        { error
+          ? <div className="interactions">
+            <p>Something went wrong.</p>
+          </div>
+          : <Table
+              list={list}
+              onDismiss={this.onDismiss}  
+            />
+        }
         <div className="interactions">
           <Button
             onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
